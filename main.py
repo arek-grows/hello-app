cd62680d01981fe93aad4737cbe38369bb6b5387
Arek T <arkadiusztruszkowski94@gmail.com>
2021-06-26 23:18:49 -0400

    sessions and cookies start

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, make_response, session, escape
 
 app = Flask(__name__)
+app.secret_key = 'any random string'
 
 
 # index
@@ -16,24 +17,24 @@ def static_files():
 
 
 # login
-@app.route('/login')
-def login():
-    return render_template('login.html', base_url=request.base_url)
-
-
-@app.route('/success/<name>')
-def success(name):
-    return 'welcome %s' % name
-
-
-@app.route('/logged_in', methods=['POST', 'GET'])
-def logged_in():
-    if request.method == 'POST':
-        user = request.form['nm']
-        return redirect(url_for('success', name=user))
-    else:
-        user = request.args.get('nm')
-        return redirect(url_for('success', name=user))
+# @app.route('/login')
+# def login():
+#     return render_template('login.html', base_url=request.base_url)
+#
+#
+# @app.route('/success/<name>')
+# def success(name):
+#     return 'welcome %s' % name
+#
+#
+# @app.route('/logged_in', methods=['POST', 'GET'])
+# def logged_in():
+#     if request.method == 'POST':
+#         user = request.form['nm']
+#         return redirect(url_for('success', name=user))
+#     else:
+#         user = request.args.get('nm')
+#         return redirect(url_for('success', name=user))
 
 
 # if statement
@@ -84,6 +85,57 @@ def fahrenheit_from(celsius):
         return str(fahrenheit)
     except ValueError:
         return "invalid input"
+
+
+# cookies
+@app.route('/cookies')
+def cookies():
+    return render_template('cookies.html')
+
+
+@app.route('/setcookie', methods=['POST', 'GET'])
+def setcookie():
+    if request.method == 'POST':
+        user = request.form['nm']
+        resp = make_response(render_template('readcookie.html'))
+        resp.set_cookie('userID', user)
+        return resp
+
+
+@app.route('/getcookie')
+def getcookie():
+    name = request.cookies.get('userID')
+    return '<h1>welcome ' + name + '</h1>'
+
+
+# sessions
+@app.route('/session')
+def sesh():
+    if 'username' in session:
+        username = session['username']
+        return 'Logged in as ' + username + '<br>' + \
+               "<b><a href = '/logout'>click here to log out</a></b>"
+    return "You are not logged in <br><a href = '/login'></b>" + \
+           "click here to log in</b></a>"
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+    <form action = "" method = "post">
+       <p><input type = "text" name = "username"/></p>
+       <p<<input type = "submit" value = "Login"/></p>
+    </form>
+    '''
+
+
+def logout():
+    # remove the username from the session if it is there
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
